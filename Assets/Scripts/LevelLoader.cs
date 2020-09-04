40d0cf62c9bac9dbc74b7bf0ba68f82960cb4b37
calvinlinardy <calvin.linardy@yahoo.com>
2020-09-05 04:03:18 +0800

    First Commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    [SerializeField] int timeToWait = 4;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForSec());
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+}
